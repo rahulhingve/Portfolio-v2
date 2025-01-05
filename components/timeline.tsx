46decfa11fd92b2dd11dfa604d2f9b6824b4adc9
rahulhingve <rahulpawar2001.rp@gmail.com>
2025-01-05 18:49:07 +0530

    added data to timeline

--- components/timeline.tsx
+++ components/timeline.tsx
@@ -5,22 +5,78 @@ import Image from "next/image";
 
 const timelineData = [
   {
-    title: "2024",
+    title: "Current - Aug 2024",
     content: (
       <div>
         <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-8">
-          Senior Software Engineer at Tech Corp, leading multiple successful projects
+          Started Learning About Web3 And similar tech  
         </p>
+        
         <div className="grid grid-cols-2 gap-4">
           <Image
-            src="/placeholder.svg?height=500&width=500"
+            src="/web3.webp?height=500&width=500"
             alt="Project 1"
             width={500}
             height={500}
             className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
           />
           <Image
-            src="/placeholder.svg?height=500&width=500"
+            src="/web31.webp?height=500&width=500"
+            alt="Project 2"
+            width={500}
+            height={500}
+            className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
+          />
+        </div>
+      </div>
+    ),
+  },
+  {
+    title: "June 2024 - Jan 2024",
+    content: (
+      <div>
+        <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-8">
+          Started Learning Full Stack Development as full time   
+        </p>
+        
+        <div className="grid grid-cols-2 gap-4">
+          <Image
+            src="/full1.webp?height=500&width=500"
+            alt="Project 1"
+            width={500}
+            height={500}
+            className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
+          />
+          <Image
+            src="/full3.webp?height=500&width=500"
+            alt="Project 2"
+            width={500}
+            height={500}
+            className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
+          />
+        </div>
+      </div>
+    ),
+  },{
+    title: "Jan 2024 - Oct 2023",
+    content: (
+      <div>
+        <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-8">
+          Frontend Developer intern  
+        </p>
+        <div className="flex gap-2 items-center text-neutral-700 dark:text-neutral-300 text-xs md:text-sm pb-2">
+           Mdp Infra Pvt Ltd Bhopal 
+          </div>
+        <div className="grid grid-cols-2 gap-4">
+          <Image
+            src="/fe.webp?height=500&width=500"
+            alt="Project 1"
+            width={500}
+            height={500}
+            className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
+          />
+          <Image
+            src="/Fe2.webp?height=500&width=500"
             alt="Project 2"
             width={500}
             height={500}
@@ -35,18 +91,18 @@ const timelineData = [
     content: (
       <div>
         <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-8">
-          Developed and launched multiple open-source projects with significant community impact
+          Graduated
         </p>
         <div className="grid grid-cols-2 gap-4">
           <Image
-            src="/placeholder.svg?height=500&width=500"
+            src="/tit4.webp?height=500&width=500"
             alt="Open Source 1"
             width={500}
             height={500}
             className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
           />
           <Image
-            src="/placeholder.svg?height=500&width=500"
+            src="/tit3.webp?height=500&width=500"
             alt="Open Source 2"
             width={500}
             height={500}
@@ -57,17 +113,33 @@ const timelineData = [
     ),
   },
   {
-    title: "2022",
+    title: "2019",
     content: (
       <div>
         <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-4">
-          Key achievements and milestones:
+          Technocrats institue of technology and Scince Bhopal 
         </p>
         <div className="mb-8">
-          <div className="flex gap-2 items-center text-neutral-700 dark:text-neutral-300 text-xs md:text-sm">
-            ✅ Led development of major feature releases
+          <div className="flex gap-2 items-center text-neutral-700 dark:text-neutral-300 text-xs md:text-sm pb-2">
+            ✅ Computer Science  Engineering 
           </div>
-          <div className="flex gap-2 items-center text-neutral-700 dark:text-neutral-300 text-xs md:text-sm">
+          <div className="grid grid-cols-2 gap-4">
+          <Image
+            src="/tit2.webp?height=500&width=500"
+            alt="Open Source 1"
+            width={500}
+            height={500}
+            className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
+          />
+          <Image
+            src="/tit1.webp?height=500&width=500"
+            alt="Open Source 2"
+            width={500}
+            height={500}
+            className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
+          />
+        </div>
+          {/* <div className="flex gap-2 items-center text-neutral-700 dark:text-neutral-300 text-xs md:text-sm">
             ✅ Contributed to core framework improvements
           </div>
           <div className="flex gap-2 items-center text-neutral-700 dark:text-neutral-300 text-xs md:text-sm">
@@ -75,7 +147,7 @@ const timelineData = [
           </div>
           <div className="flex gap-2 items-center text-neutral-700 dark:text-neutral-300 text-xs md:text-sm">
             ✅ Spoke at tech conferences
-          </div>
+          </div> */}
         </div>
       </div>
     ),
