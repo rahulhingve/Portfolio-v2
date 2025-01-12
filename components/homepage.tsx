7d59f2c62a1be7effd5a27e7f77d19df2da32ee2
rahulhingve <rahulpawar2001.rp@gmail.com>
2025-01-12 11:10:28 +0530

    ui impv

--- components/homepage.tsx
+++ components/homepage.tsx
@@ -35,24 +35,7 @@ export function Homepage({id}:any) {
           animate="visible"
           className=" h-screen flex flex-col p-4 items-center justify-center"
         >
-          <motion.h1 variants={itemVariants} className="text-4xl font-bold mb-4">
-            Still Working on this site, Please Visit the Old Portfolio Website But still you can download my Resume.
-          </motion.h1>
-          <motion.div
-          className="pb-8"
-            variants={itemVariants}
-            whileHover={{ scale: 1.05 }}
-            whileTap={{ scale: 0.95 }}
-          >
-            <a
-              href="https://rahulhingvev1.vercel.app/"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="bg-blue-500 text-white px-6 py-2 rounded-full inline-block transition-colors hover:bg-blue-600"
-            >
-             Visit 
-            </a>
-          </motion.div>
+        
           <motion.h1 variants={itemVariants} className="text-4xl font-bold mb-4">
             Software Engineer, Techie & Open-Source Maintainer
           </motion.h1>
