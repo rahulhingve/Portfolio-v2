31eaa1a20deefabb5ec70d88fdfba14a65217fe2
rahulhingve <rahulpawar2001.rp@gmail.com>
2025-03-30 15:58:16 +0530

    updated SEO keyWords

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
-import { FloatingNav } from "@/components/ui/floating-navbar";
-import { Footer } from "@/components/footer";
-import { IconHome, IconCode, IconTimeline, IconMail } from "@tabler/icons-react";
+
 
 const inter = Inter({ subsets: ["latin"] });
 
